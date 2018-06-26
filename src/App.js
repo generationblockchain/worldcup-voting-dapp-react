5965f0acef3c2f33ff561967e9c2fa5b592fb6a7
Aayush Kapoor <xeoneux@icloud.com>
2018-06-26 12:45:32 +0530

    Add random flags for display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { FlagIcon } from 'react-flag-kit';
+import { FlagIcon } from 'react-flag-kit'
+import Overdrive from 'react-overdrive'
 
 // import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 // import getWeb3 from './utils/getWeb3'
@@ -9,11 +10,14 @@ import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
+const flags = ['IN', 'GB', 'US', 'CN', 'TV', 'IO', 'VN', 'IT']
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      flagSelected: '',
       storageValue: 0,
       web3: null
     }
@@ -22,13 +26,11 @@ class App extends Component {
   componentWillMount() {
     // Get network provider and web3 instance.
     // See utils/getWeb3 for more info.
-
     // getWeb3
     //   .then(results => {
     //     this.setState({
     //       web3: results.web3
     //     })
-
     //     // Instantiate contract once web3 provided.
     //     this.instantiateContract()
     //   })
@@ -44,21 +46,17 @@ class App extends Component {
      * Normally these functions would be called in the context of a
      * state management library, but for convenience I've placed them here.
      */
-
     // const contract = require('truffle-contract')
     // const simpleStorage = contract(SimpleStorageContract)
     // simpleStorage.setProvider(this.state.web3.currentProvider)
-
     // // Declaring this for later so we can chain functions on SimpleStorage.
     // var simpleStorageInstance
-
     // // Get accounts.
     // this.state.web3.eth.getAccounts((error, accounts) => {
     //   simpleStorage
     //     .deployed()
     //     .then(instance => {
     //       simpleStorageInstance = instance
-
     //       // Stores a given value, 5 by default.
     //       return simpleStorageInstance.set(5, { from: accounts[0] })
     //     })
@@ -76,28 +74,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-          <a href="#" className="pure-menu-heading pure-menu-link">
-            Truffle Box
-          </a>
-        </nav>
-
         <main className="container">
           <div className="pure-g">
+            <div className="pure-u-1-1">LOGO</div>
+
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <FlagIcon code="IN" size={96} />
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>
-                If your contracts compiled and migrated successfully, below will
-                show a stored value of 5 (by default).
-              </p>
-              <p>
-                Try changing the value stored on <strong>line 59</strong> of
-                App.js.
-              </p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <div className="pure-u-1-6" />
+              <div className="pure-u-2-3">
+                {flags.map(flag => (
+                  <div key={flag} className="pure-u-1-4 genb-flag">
+                    <FlagIcon code={flag} size={256} />
+                  </div>
+                ))}
+              </div>
+              <div className="pure-u-1-6" />
+
+              {this.state.flagSelected ? (
+                <Overdrive id="content">
+                  <div>
+                    <FlagIcon code="IN" size={96} />
+                  </div>
+                </Overdrive>
+              ) : (
+                <Overdrive id="content">
+                  <div style={{ position: 'fixed', right: '5px' }}>
+                    <FlagIcon code="US" size={96} />
+                  </div>
+                </Overdrive>
+              )}
             </div>
           </div>
         </main>
